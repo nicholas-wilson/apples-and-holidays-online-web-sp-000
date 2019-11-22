21a1cf7db4b1dd02a02e969cf27fc022972dcb67
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-22 04:29:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,15 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-
+  holiday_hash.each do |season, holidays|
+    if season == :summer
+      holidays.each do |holiday, events|
+        if holiday == :fourth_of_july
+          return events[1]
+        end
+      end
+    end
+  end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
