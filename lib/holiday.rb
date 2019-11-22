0089a3684728549fe3526b1afedbc3ccf849b0d1
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-22 05:02:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts "#{holidays.to_s.capitalize}:"
+    puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supplies_for|
       print "\t#{holiday.to_s.capitalize}:"
       supplies_for.each_index do |supply|
