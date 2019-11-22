6bc1a756895675c7f86c8842f4760f1a92d6928f
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-22 04:50:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,5 +82,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holidays_with_bbqs = []
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies_for|
+      supplies_for.each do |supply|
+        if supply == "BBQ"
+          holidays_with_bbqs << holiday
+      end
+    end
+  end
 end
